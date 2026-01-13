a8e96a5208c1cbe64a4b59dd59c2f40d155db8c2
Vilém Zouhar <vilem.zouhar@gmail.com>
2026-01-13 14:26:27 +0100

    fix negative score and normalization

--- scripts/01-get_data_text.py
+++ scripts/01-get_data_text.py
@@ -5,18 +5,33 @@ import sklearn.model_selection
 
 os.makedirs("data", exist_ok=True)
 
+def fix_score(x):
+    if x >= 1:
+        x = x
+    elif x < 0:
+        x = max(0, 100 + 4*x)
+    else:
+        x = 100 * x
+    x = max(min(x, 100), 0)
+    return x
+
 data_all = [
     {
         "src": x["src"],
         "mt": x["tgt"][sys],
-        "score": x["scores"][sys]["human"] if x["scores"][sys]["human"] >= 1 else 100*x["scores"][sys]["human"],
+        "score": fix_score(x["scores"][sys]["human"]),
         "src_audio": "",
     }
-    for data in (subset2evaluate.utils.load_data_wmt_all() | subset2evaluate.utils.load_data_biomqm()).values()
+    for name, data in (subset2evaluate.utils.load_data_wmt_all(normalize=False) | subset2evaluate.utils.load_data_biomqm(normalize=False)).items()
     for x in data
     if "speech" not in x["domain"]
     for sys in x["tgt"]
 ]
+
+# import matplotlib.pyplot as plt
+# plt.hist([x["score"] for x in data_all])
+# plt.show()
+# exit()
 
 data_train, data_dev = sklearn.model_selection.train_test_split(data_all, test_size=1000)
 
